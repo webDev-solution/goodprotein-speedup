f820bf0a8dcebd25b1566394159c610bfdc2c9a7
bStar928 <brightstar928@hotmail.com>
2025-04-06 22:39:36 -0400

    fix carousel issue on iphone

--- assets/custom-featured-products-carousel.js
+++ assets/custom-featured-products-carousel.js
@@ -1,31 +1,36 @@
 class ProductCarouselComponent extends HTMLElement {
   constructor() {
     super();
-    this.init();
+//    this.init();
+  }
+  connectedCallback() {
+    document.addEventListener("DOMContentLoaded", () => this.init());
   }
   init() {
-    $(this).slick({
-      infinite: true,
-      slidesToShow: 5,
-      slidesToScroll: 1,
-      centerMode: true,
-      centerPadding: '0px',
-      arrows:true,
-      prevArrow:'<button type="button" class="slick-prev"><i class="fa-solid fa-chevron-left"></i></button>',
-      nextArrow:'<button type="button" class="slick-next"><i class="fa-solid fa-chevron-right"></i></button>',
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            centerPadding: '80px',
-            infinite: true
+    const $carousel = $(this.querySelector(".carousel"));
+    if ($carousel.length) {
+      $carousel.slick({
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        centerMode: true,
+        centerPadding: '0px',
+        arrows:true,
+        prevArrow:'<button type="button" class="slick-prev"><i class="fa-solid fa-chevron-left"></i></button>',
+        nextArrow:'<button type="button" class="slick-next"><i class="fa-solid fa-chevron-right"></i></button>',
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              centerPadding: '80px',
+              infinite: true
+            }
           }
-        }
-      ]
-    });
+        ]
+      });
+    }
   }
 }
-
 customElements.define('product-carousel', ProductCarouselComponent);
